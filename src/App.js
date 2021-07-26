54238c79eb31729009f0490857c4cb7e95fb20bb
Chris King <cking.js@gmail.com>
2021-07-26 12:05:28 -0400

    quick edit for soaring sun placeholder

--- src/App.js
+++ src/App.js
@@ -7,15 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Placeholder for Soaring Sun, LLC. website.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://soaringsunllc.com"
         >
-          Learn React
+          Soaring Sun, LLC.
         </a>
       </header>
     </div>
